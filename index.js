090b26848fc9b80368ff59d8b3eebdcff2a5a4cc
Nell5 <neeennever@gmail.com>
2024-05-24 15:48:30 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log();
+
+let randomNumbers = Math.round(Math.random() * 100);
+
+alert(`Bu sizni ${randomNumbers} random son`);
+
+confirm(`Siz ${randomNumbers} bu sonni 2ga kopaytirishga va bolish`);
+
+alert(`Kopaytirganda: ${randomNumbers} * 2 = ${randomNumbers * 2} \n
+
+Bolganda: ${randomNumbers} 2 = ${randomNumbers / 2} \n
+
+Qoldiq: ${randomNumbers} % 2 = ${randomNumbers % 3} \n`);
+
+
+
+
+
+
+
+
+
